acec6baa4deda8cc1a0551b30d74b04d3e88b0b7
Benian Toksoez <btoksoez@c2r4s6.42lisboa.com>
2024-02-07 16:44:46 +0000

    ll

--- pipex/shell_return.c
+++ pipex/shell_return.c
@@ -6,7 +6,7 @@
 /*   By: btoksoez <btoksoez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 13:43:15 by btoksoez          #+#    #+#             */
-/*   Updated: 2024/02/07 14:27:27 by btoksoez         ###   ########.fr       */
+/*   Updated: 2024/02/07 15:12:25 by btoksoez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,9 @@
 
 int shell_return(char *cmd, char *envp[])
 {
-	int	pipe_fd[2]; //fd[1] = write, fd[0] = read
 	int	shellpid1;
-	int	shellpid2;
-	char	exit_code[10];
-	int		res;
+	int	status;
 
-	if (pipe(pipe_fd) == -1)
-	{
-		perror("Error opening Shell Pipe");
-		exit(EXIT_FAILURE);
-	}
 	shellpid1 = fork();
 	if (shellpid1 < 0)
 	{
@@ -33,30 +25,24 @@ int shell_return(char *cmd, char *envp[])
 	}
 	else if (shellpid1 == 0)	//child process for first command
 	{
-		close(pipe_fd[0]); //close read end of pipe
-		dup2(pipe_fd[1], STDOUT_FILENO); //reroute stdout to write end of pipe
-		close(pipe_fd[1]);
-		//execve("/bin/sh", (char *[]){"/bin/sh", "-c", cmd, "|", "echo $?", NULL}, envp);
+		execve("/bin/sh", (char *[]){"/bin/sh", "-c", cmd, NULL}, envp);
 		perror("Shell Execve1 failed");
 		exit(EXIT_FAILURE);
 	}
-
 	else
 	{
-		waitpid(shellpid1, NULL, 0);
-		close(pipe_fd[1]); //close write end of pipe
-		if (read(pipe_fd[0], exit_code, sizeof(exit_code)) < 0)
-			return (-1); //error with read
-		close(pipe_fd[0]);
-		res = ft_atoi(exit_code);
-		return (res);
+		waitpid(shellpid1, &status, 0);
+		return (status);
 	}
 }
 
 int main()
 {
-    char *cmd = "hello"; // Example command
+    char *cmd = "cat"; // Example command
     int exit_code = shell_return(cmd, NULL);
     printf("Exit code of '%s': %d\n", cmd, exit_code);
     return 0;
 }
+
+
+//check if it returns 0 for all valid commands, and else otherwise
